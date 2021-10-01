5639b5c4dcbf11d55ffbf89c5c247e05aec3eca0
Gian Yim <57858695+210577@users.noreply.github.com>
2021-10-01 16:45:15 +0900

    Update XOR 2.py
    Added comments

--- 수학-응용-문제해결/XOR 2.py
+++ 수학-응용-문제해결/XOR 2.py
@@ -1,2 +1,12 @@
-(key,plaintext,cipher) = ('cipher', 'Each character on a computer is assigned a unique code.', [])
+(key,plaintext) = ('cipher', 'Each character on a computer is assigned a unique code.') # 키와 평문을 동시에 입력받기
 print(list(map(lambda x, y: int(ord(y)^ord(x)), key*(len(plaintext)//len(key))+key[0:len(plaintext)%len(key)], plaintext)))
+
+# lambda, map을 이용해 평문과 키를 평문과 같은 길이가 될 때까지 반복한 배열에서 한 글자씩 차례대로 XOR을 수행함
+#
+# 사실상 다음과 동일하다:
+# cipher = []
+# for i in range(len(plaintext)):
+#    x = plaintext[i]
+#    y = key[i%len(key)]
+#    cipher.append(int(ord(y)^ord(x)))
+# print(cipher)
