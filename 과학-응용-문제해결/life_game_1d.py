ce0c0092a1dce4fd70b5b558889ba78dfa66b82c
Gian Yim <57858695+210577@users.noreply.github.com>
2021-10-01 15:52:00 +0900

    Update life_game_1d.py
    Added comments

--- 과학-응용-문제해결/life_game_1d.py
+++ 과학-응용-문제해결/life_game_1d.py
@@ -1,13 +1,15 @@
-data = input().split(',')
-data[0] = data[0][1:-1]
+#1차원 생명게임 구현하기
+
+data = input().split(',') # 초기 상태 및 구하고자 하는 세대가 몇 번쨰인지를 '0110011', 1 과 같이 입력받는다
+data[0] = data[0][1:-1] # 문자열 '0110011' 에서 양쪽에 있는 '를 제거한다
 data[1] = int(data[1].strip())
 
-def iteration(data):
+def iteration(data): # 1세대 후의 배치 구하기
     result = ''
     for i in range(len(data)):
-        result += str(int(data[i])^((int(data[i-1])^int(data[(i+1)%len(data)]))))
+        result += str(int(data[i])^((int(data[i-1])^int(data[(i+1)%len(data)])))) # 다음 세대의 i번째 칸은 이전 세대의 (i-1)번째 세대와 (i+1)번쨰 세대의 XOR 값이다.
     return result
 
 for i in range(data[1]):
-    data[0] = iteration(data[0])
+    data[0] = iteration(data[0]) # 원하는 세대를 얻을 때까지 반복
 print(data[0])
