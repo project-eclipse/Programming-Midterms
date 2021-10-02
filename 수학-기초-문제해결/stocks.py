ec3f1f3eb037c17115f6b95f6cd1e39b19b82b3a
Ryang Sohn <loop.infinitely@gmail.com>
2021-10-02 14:42:39 +0900

    Fix a bug in stocks.py

--- 수학-기초-문제해결/stocks.py
+++ 수학-기초-문제해결/stocks.py
@@ -4,7 +4,7 @@ days = int(input("투자 일수 : "))
 rate = list(map(int, input("변동 폭 : ").split()))
 
 for i in rate:
-    total *= (1 + rate/100)
+    total *= 1 + i / 100
     total = int(total)
 
 print(total - price)
